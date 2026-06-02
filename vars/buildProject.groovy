4b863e377c4117953d124eb24e3e6a28a884d66c
rajeshkosta <155640944+rajeshkosta@users.noreply.github.com>
2026-06-02 18:13:22 +0530

    Update buildProject.groovy

--- vars/buildProject.groovy
+++ vars/buildProject.groovy
@@ -13,15 +13,31 @@ def call() {
             '''
             break
 
-        case "python":
-            sh '''
-            docker run --rm \
-                -v $PWD:/app \
-                -w /app \
-                python:3.12 \
-                sh -c "pip install -r requirements.txt && pytest"
-            '''
-            break
+        def call() {
+
+    // detect python project location
+    def pythonRoot = ""
+
+    if (fileExists("requirements.txt")) {
+        pythonRoot = "."
+    } 
+    else if (fileExists("Application-Code/requirements.txt")) {
+        pythonRoot = "Application-Code"
+    } 
+    else {
+        error "requirements.txt not found"
+    }
+
+    echo "Python project root: ${pythonRoot}"
+
+    sh """
+    docker run --rm \
+    -v \$PWD:/app \
+    -w /app/${pythonRoot} \
+    python:3.12 \
+    sh -c "pip install -r requirements.txt && pytest"
+    """
+}
 
         case "go":
             sh '''
