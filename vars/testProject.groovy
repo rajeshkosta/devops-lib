597cf39d0ad97e483305a6cce983b09ff9527d7a
rajeshkosta <155640944+rajeshkosta@users.noreply.github.com>
2026-06-02 17:55:57 +0530

    Update testProject.groovy

--- vars/testProject.groovy
+++ vars/testProject.groovy
@@ -13,7 +13,7 @@ def call() {
         case "python":
             sh '''
             docker run --rm \
-                -v ${WORKSPACE}:/app \
+                -v $PWD:/app \
                 -w /app \
                 python:3.12 \
                 bash -c "pip install -r requirements.txt && pytest"
