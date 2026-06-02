92fcc047dba4ad5f868f971ad8fbc9b2c2931ad9
rajeshkosta <155640944+rajeshkosta@users.noreply.github.com>
2026-06-02 18:23:53 +0530

    Update buildProject.groovy

--- vars/buildProject.groovy
+++ vars/buildProject.groovy
@@ -2,9 +2,41 @@ def call() {
 
     switch(env.LANG) {
 
+        case "python":
+
+            def pythonRoot = ""
+
+            // check root first
+            if (fileExists("requirements.txt")) {
+                pythonRoot = "."
+            }
+            // check Application-Code folder
+            else if (fileExists("Application-Code/requirements.txt")) {
+                pythonRoot = "Application-Code"
+            }
+            else {
+                error "requirements.txt not found in root or Application-Code"
+            }
+
+            echo "Detected Python project location: ${pythonRoot}"
+
+            sh """
+            docker run --rm \
+            -v \$PWD:/app \
+            -w /app/${pythonRoot} \
+            python:3.12 \
+            sh -c "
+                pip install -r requirements.txt && pytest
+            "
+            """
+
+            break
+
+
         case "java":
             sh 'mvn clean package -DskipTests'
             break
+
 
         case "node":
             sh '''
@@ -13,30 +45,6 @@ def call() {
             '''
             break
 
-        case "python":
-
-            def pythonRoot = ""
-
-            if (fileExists("requirements.txt")) {
-                pythonRoot = "."
-            } 
-            else if (fileExists("Application-Code/requirements.txt")) {
-                pythonRoot = "Application-Code"
-            } 
-            else {
-                error "requirements.txt not found"
-            }
-
-            echo "Python project root: ${pythonRoot}"
-
-            sh """
-            docker run --rm \
-            -v \$PWD:/app \
-            -w /app/${pythonRoot} \
-            python:3.12 \
-            sh -c "pip install -r requirements.txt && pytest"
-            """
-            break
 
         case "go":
             sh '''
@@ -44,6 +52,7 @@ def call() {
             go build -o app
             '''
             break
+
 
         default:
             error "Unsupported language: ${env.LANG}"
