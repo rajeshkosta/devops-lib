16e4ba090600c09a4dd7ff586a80067071935ec9
rajeshkosta <155640944+rajeshkosta@users.noreply.github.com>
2026-06-02 17:30:45 +0530

    Update buildProject.groovy

--- vars/buildProject.groovy
+++ vars/buildProject.groovy
@@ -14,7 +14,12 @@ def call() {
             break
 
         case "python":
-            sh 'python3 -m pip install -r requirements.txt'
+            sh '''
+            python3 -m venv venv
+            . venv/bin/activate
+            pip install --upgrade pip
+            pip install -r requirements.txt
+            '''
             break
 
         case "go":
